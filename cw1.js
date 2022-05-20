43a52fa6b4c91dc81eb9cc1f96ae67e67fe3dd5c
MashaBogdanova <105852636+MashaBogdanova@users.noreply.github.com>
2022-05-20 16:46:24 +0200

    Create cw1.js

---
+++ cw1.js
@@ -0,0 +1,11 @@
+var countSheep = function (input){
+if (input == 0){
+   return("");
+  }
+else {
+  let sheeps = "";
+  for (let i=1; i<=input; i++){
+    sheeps += i + " sheep..."
+  }
+  return sheeps;
+}
